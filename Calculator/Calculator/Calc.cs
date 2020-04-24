c2171d1253b68e16ae0a0a11c44298a8fd3e2d89
Nikita Tyulkov <tyulkovnikita@gmail.com>
2020-04-24 21:57:13 +0300

    Calculator task

---
+++ Calculator/Calculator/Calc.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorManager
+    {
+        private List<string> operators;
+        private List<string> standart_operators = new List<string>(new string[] { "(", ")", "+", "-", "*", "/" });
+
+        public CalculatorManager(params string[] operatorList)
+        {
+            if (operatorList.Length == 0)
+                operators = new List<string>(standart_operators);
+            else
+                operators = new List<string>(operatorList);
+
+        }
+
+        private IEnumerable<string> Separate(string input)
+        {
+            List<string> inputSeparated = new List<string>();
+            int pos = 0;
+            while (pos < input.Length)
+            {
+                string s = string.Empty + input[pos];
+                if (!standart_operators.Contains(input[pos].ToString()))
+                {
+                    if (Char.IsDigit(input[pos]))
+                        for (int i = pos + 1; i < input.Length &&
+                            (Char.IsDigit(input[i]) || input[i] == ',' || input[i] == '.'); i++)
+                            s += input[i];
+                    else if (Char.IsLetter(input[pos]))
+                        for (int i = pos + 1; i < input.Length &&
+                            (Char.IsLetter(input[i]) || Char.IsDigit(input[i])); i++)
+                            s += input[i];
+                }
+                yield return s;
+                pos += s.Length;
+            }
+        }
+
+        private byte GetOperationPriority(string s)
+        {
+            switch (s)
+            {
+                case "(":
+                    return 1;
+                case ")":
+                    return 2;
+                case "+":
+                    return 3;
+                case "-":
+                    return 4;
+                case "*":
+                case "/":
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
+
+        string ReplaceMinus(string input)
+        {
+            StringBuilder result = new StringBuilder(input.Length);
+            int start = 0;
+
+            if (input[0] == '-')
+            {
+                for (int j = 1; j < input.Length; j++)
+                {
+                    if (operators.Contains(input[j].ToString()))
+                    {
+                        var tmp = input.Substring(0, j);
+                        result.Append("0" + tmp);
+                        start = j;
+                        break;
+                    }
+                    if (j == input.Length - 1)
+                    {
+                        var tmp = input.Substring(0);
+                        result.Append("(0" + tmp + ")");
+                    }
+                }
+            }
+
+            for (int i = start; i < input.Length; i++)
+            {
+                if (input[i] == '-' && input[i - 1] != ')' && operators.Contains(input[i - 1].ToString()))
+                {
+                    for (int j = i + 1; j < input.Length; j++)
+                    {
+                        if (operators.Contains(input[j].ToString()))
+                        {
+                            var tmp = input.Substring(i, j - i);
+                            result.Append("0" + tmp + ")");
+                            i = j;
+                            break;
+                        }
+                        if (j == input.Length - 1)
+                        {
+                            var tmp = input.Substring(i);
+                            result.Append("0" + tmp + ")");
+                            i = j;
+                        }
+                    }
+                }
+                else
+                    result.Append(input[i]);
+            }
+
+            return result.ToString();
+        }
+
+        public string[] ConvertToPostfixNotation(string input)
+        {
+            List<string> outputSeparated = new List<string>();
+            Stack<string> stack = new Stack<string>();
+
+            input = ReplaceMinus(input.Trim());
+
+            foreach (string c in Separate(input))
+            {
+                if (operators.Contains(c))
+                {
+                    if (stack.Count > 0 && !c.Equals("("))
+                    {
+                        if (c.Equals(")"))
+                        {
+                            string s = stack.Pop();
+                            while (s != "(")
+                            {
+                                outputSeparated.Add(s);
+                                s = stack.Pop();
+                            }
+                        }
+                        else if (GetOperationPriority(c) >= GetOperationPriority(stack.Peek()))
+                            stack.Push(c);
+                        else
+                        {
+                            while (stack.Count > 0 && GetOperationPriority(c) < GetOperationPriority(stack.Peek()))
+                                outputSeparated.Add(stack.Pop());
+                            stack.Push(c);
+                        }
+                    }
+                    else
+                        stack.Push(c);
+                }
+                else
+                    outputSeparated.Add(c);
+            }
+            if (stack.Count > 0)
+                foreach (string c in stack)
+                    outputSeparated.Add(c);
+
+            return outputSeparated.ToArray();
+        }
+
+
+        public static string Calculate(params string[] values)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            string[] operation = new string[] { "/", "*", "+", "-" };
+
+            foreach (var item in values)
+            {
+                if (operation.Contains(item))
+                {
+                    double a;
+                    double b;
+
+                    string sb = stack.Pop();
+                    string sa = stack.Pop();
+
+                    if (!double.TryParse(sa, out a) | !double.TryParse(sb, out b)) { Console.WriteLine("Error!!!"); }
+
+                    if (item == "/") stack.Push((a / b).ToString());
+                    if (item == "*") stack.Push((a * b).ToString());
+                    if (item == "+") stack.Push((a + b).ToString());
+                    if (item == "-") stack.Push((a - b).ToString());
+                }
+                else
+                {
+                    stack.Push(item);
+                }
+            }
+
+            return stack.Pop();
+        }
+    }
+
+    
+}
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        string input = ""; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "1";
+                input = "1";
+            }
+            else
+            {
+                textBox_Result.Text += "1";
+                input += "1";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "4";
+                input = "4";
+            }
+            else
+            {
+                textBox_Result.Text += "4";
+                input += "4";
+            }
+        }
+
+        private void buttonOne_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "7";
+                input = "7";
+            }
+            else
+            {
+                textBox_Result.Text += "7";
+                input += "7";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "8";
+                input = "8";
+            }
+            else
+            {
+                textBox_Result.Text += "8";
+                input += "8";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "5";
+                input = "5";
+            }
+            else
+            {
+                textBox_Result.Text += "5";
+                input += "5";
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "2";
+                input = "2";
+            }
+            else
+            {
+                textBox_Result.Text += "2";
+                input += "2";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "9";
+                input = "9";
+            }
+            else
+            {
+                textBox_Result.Text += "9";
+                input += "9";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "6";
+                input = "6";
+            }
+            else
+            {
+                textBox_Result.Text += "6";
+                input += "6";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "3";
+                input = "3";
+            }
+            else
+            {
+                textBox_Result.Text += "3";
+                input += "3";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text != "0" && textBox_Result.Text != null)
+            {
+                textBox_Result.Text += "/";
+                input += "/";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text != "0" && textBox_Result.Text != null)
+            {
+                textBox_Result.Text += "*";
+                input += "*";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "-";
+                input = "-";
+            }
+            else
+            {
+                textBox_Result.Text += "-";
+                input += "-";
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text != "0" && textBox_Result.Text != null)
+            {
+                textBox_Result.Text += "+";
+                input += "+";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text == "0" && textBox_Result.Text != null || input == "")
+            {
+                textBox_Result.Text = "(";
+                input = "(";
+            }
+            else
+            {
+                textBox_Result.Text += "(";
+                input += "(";
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text != "0" && textBox_Result.Text != null)
+            {
+                textBox_Result.Text += "0";
+                input += "0";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox_Result.Text != "0" && textBox_Result.Text != null)
+            {
+                textBox_Result.Text += ")";
+                input += ")";
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            CalculatorManager parser = new CalculatorManager();
+            string[] notation = parser.ConvertToPostfixNotation(input);
+            string result = CalculatorManager.Calculate(notation);
+            label1.Text = input + " = " + result;
+            textBox_Result.Text = "0";
+            input = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+            input = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (input.Length == 1 || input.Length == 0)
+            {
+                textBox_Result.Text = "0";
+                input = "";
+            }
+            else
+            {
+                textBox_Result.Text = input.Remove(input.Length - 1);
+                input = input.Remove(input.Length - 1);
+            }
+        }
+    }
+}
+
